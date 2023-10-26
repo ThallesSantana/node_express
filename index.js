6b34a436647890085d65ca2661de5150339b22c2
Thalles <thalles3008@gmail.com>
2023-10-26 14:55:30 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const path = require("path")
+const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
-const app = express()
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    console.log("Acessando dados do usuário:", id)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) =>{
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
